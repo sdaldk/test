01cc5accb83db19dffa0181ce8dd236e884022b3
sdaldk <114871365+sdaldk@users.noreply.github.com>
2023-11-23 23:41:29 +0800

    Update Readers.java

--- Readers.java
+++ Readers.java
@@ -5,10 +5,4 @@ import java.util.List;
 public class Readers {
     public static String UserName;
     public static String LoginPassword;
-<<<<<<< HEAD
-=======
-    public static int SaveStatus;
-    public static String LoginToken;
-    public static String photo;
->>>>>>> 601f70d73c6914f39b61e52ae9660815d3e76d00
 }
