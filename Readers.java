763c05c9f7c97cad335cf2a27f38b1b9fe13a1d0
zjh2021140117 <3261658507@qq.com>
2023-11-23 22:28:53 +0800

    ss

--- Readers.java
+++ Readers.java
@@ -5,6 +5,4 @@ import java.util.List;
 public class Readers {
     public static String UserName;
     public static String LoginPassword;
-    public static int SaveStatus;
-    public static String LoginToken;
 }
