148bf9047405a7004b1ab384d8ab79f07164830e
sdaldk <114871365+sdaldk@users.noreply.github.com>
2023-11-23 19:30:03 +0800

    Add files via upload

---
+++ Readers.java
@@ -0,0 +1,17 @@
+package Readers;
+
+import java.util.List;
+
+public class Readers {
+    public static String UserName;
+    public static String LoginPassword;
+    public static int SaveStatus;
+    public static String LoginToken;
+    public static long expirationTime;
+    public static String photo;
+    public static String nickname;
+    public static List<String> borrowBook;
+    public static List<String> returnBack;
+    public static int position;
+    public static String message;
+}
